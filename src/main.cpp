bfcdbc9afe6c1511b988c59a35e78a26126e1df6
Ariston Lorenzo <me@ariston.dev>
2023-04-12 14:19:52 -0400

    Use SetStaticPages() to serve public/
    It makes it easier to serve everything without having
    to keep using server.Get() to serve everything.
    
    Signed-off-by: Ariston Lorenzo <me@ariston.dev>

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@ int main() {
 	Link::Server server;
 	server.SetPort(3000);
 
+	server.SetStaticPages("../public/");
+
   	// 404 Page
   	server.Error(404, [](Link::Request* req, Link::Response* res) {
   		res->SetStatus(404)->SetBody("404 Not Found");
@@ -15,13 +17,6 @@ int main() {
   	server.Get("/", [](Link::Request* req, Link::Response* res) {
     	res->SetHeader("Content-Type", "text/html; charset=UTF-8");
 		std::ifstream file("../www/index.html");
-		std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-		res->SetBody(content);
-  	});
-
-  	server.Get("/css/index.css", [](Link::Request* req, Link::Response* res) {
-   		res->SetHeader("Content-Type", "text/css; charset=UTF-8");
-		std::ifstream file("www/css/index.css");
 		std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 		res->SetBody(content);
   	});
